e6cbf12d111bfb74aa230c6eb2fe4e5a2e060ed5
nummyn0rih <nummyn0rih@gmail.com>
2023-11-17 23:08:11 +0300

    feat: добавил оболочку приложения (comp Shell)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import { Shell } from "./components";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider theme={theme}><Shell/></MantineProvider>;
 }
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { Shell } from './Shell';
+
+export {
+    Shell
+}
