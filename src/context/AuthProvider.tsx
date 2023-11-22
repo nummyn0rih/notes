6441b1a68171ec9130385f68ff1ca0b6cf507e4e
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-11-22 16:55:00 +0300

    feat(src/context): добавил AuthProvider и auth

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,80 @@
+import { ReactNode, createContext, useContext, useState } from 'react';
+import {
+  useNavigate,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+import { fakeAuthProvider } from "./auth";
+
+interface AuthContextType {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  user: any;
+  signin: (user: string, callback: VoidFunction) => void;
+  signout: (callback: VoidFunction) => void;
+}
+
+const AuthContext = createContext<AuthContextType>(null!);
+
+function AuthProvider({ children }: { children: ReactNode }) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [user, setUser] = useState<any>(null);
+
+  const signin = (newUser: string, callback: VoidFunction) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback();
+    });
+  };
+
+  const signout = (callback: VoidFunction) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback();
+    });
+  };
+
+  const value = { user, signin, signout };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+// function AuthStatus() {
+//   const auth = useAuth();
+//   const navigate = useNavigate();
+
+//   if (!auth.user) {
+//     return <p>You are not logged in.</p>;
+//   }
+
+//   return (
+//     <p>
+//       Welcome {auth.user}!{" "}
+//       <button
+//         onClick={() => {
+//           auth.signout(() => navigate("/"));
+//         }}
+//       >
+//         Sign out
+//       </button>
+//     </p>
+//   );
+// }
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const auth = useAuth();
+  const location = useLocation();
+
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
