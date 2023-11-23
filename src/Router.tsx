08636259475cd075b1bd7effa0223c86362e7e92
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-11-23 16:58:45 +0300

    feat(src/Router): добавил страницу 404

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,27 +1,26 @@
 // import { lazy } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './layout/Layout';
-import { Shell } from './components';
+import { NotFound, Shell } from './components';
 import { LoginPage } from './pages/LoginPage';
+import { RequireAuth } from './context/AuthProvider';
 
 const router = createBrowserRouter([
   {
     id: "root",
     path: "/",
-    // loader() {
-      // Our root route always provides the user, if logged in
-      // return { user: fakeAuthProvider.username };
-    // },
     Component: Layout,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
-        Component: Shell,
+        Component: RequireAuth,
+        // children: [
+
+        // ]
       },
       {
         path: "login",
-        // action: loginAction,
-        // loader: loginLoader,
         Component: LoginPage,
       },
     //   {
