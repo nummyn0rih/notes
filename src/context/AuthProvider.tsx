406595cf3cf0f38cbb7a103b0b00b3963c56275d
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-11-24 12:47:37 +0300

    fix(src/context): исправил тип children

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -39,6 +39,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useAuth() {
   return useContext(AuthContext);
 }
@@ -65,15 +66,11 @@ export function useAuth() {
 //   );
 // }
 
-export function RequireAuth({ children }) {
+export function RequireAuth({ children }: { children?: JSX.Element }) {
   const auth = useAuth();
   const location = useLocation();
 
   if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
