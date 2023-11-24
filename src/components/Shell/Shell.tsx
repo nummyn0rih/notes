08256f800d74810a51fbb63e3118d01ec3464267
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-11-24 14:31:05 +0300

    fix(src/components): убрал футер, асайд доб скролл

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -1,37 +1,40 @@
 import { useDisclosure } from '@mantine/hooks';
-import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { AppShell, ScrollArea, Burger, Group, Skeleton } from '@mantine/core';
 import { MantineLogo } from '@mantine/ds';
+import { Outlet } from 'react-router-dom';
 
 export function Shell() {
-    const [opened, { toggle }] = useDisclosure();
+  const [opened, { toggle }] = useDisclosure();
 
-    return (
-        <AppShell
-            header={{ height: 60 }}
-            footer={{ height: 60 }}
-            navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-            aside={{ width: 300, breakpoint: 'md', collapsed: { desktop: false, mobile: true } }}
-            padding="md"
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <MantineLogo size={30} />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        <ScrollArea
+          mr={-10}
+          offsetScrollbars
+          scrollbarSize={10}
+          scrollHideDelay={500}
         >
-            <AppShell.Header>
-                <Group h="100%" px="md">
-                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-                    <MantineLogo size={30} />
-                </Group>
-            </AppShell.Header>
-            <AppShell.Navbar p="md">
-                Navbar
-                {Array(15)
-                    .fill(0)
-                    .map((_, index) => (
-                        <Skeleton key={index} h={28} mt="sm" animate={false} />
-                    ))}
-            </AppShell.Navbar>
-            <AppShell.Main>
-                Aside is hidden on on md breakpoint and cannot be opened when it is collapsed
-            </AppShell.Main>
-            <AppShell.Aside p="md">Aside</AppShell.Aside>
-            <AppShell.Footer p="md">Footer</AppShell.Footer>
-        </AppShell>
-    );
+          {Array(25)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+        </ScrollArea>
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Outlet/>
+      </AppShell.Main>
+    </AppShell>
+  );
 }
