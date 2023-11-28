9f08b3278eb1aac2b12d30cb57e3944dc595bbd4
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-11-28 16:04:28 +0300

    feat: вынес заметку в отдельный компонент Note

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import { Shell } from './Shell';
 import { AuthenticationForm } from './AuthenticationForm';
 import { NotFound } from './NotFound';
 import { Header } from './Header';
+import { Note } from './Note';
 
 export {
     Shell,
     AuthenticationForm,
     NotFound,
-    Header
+    Header,
+    Note
 }
