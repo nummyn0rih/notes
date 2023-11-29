ec4ddae2f8e1f136486864680822b79f2b288a66
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-11-29 16:46:45 +0300

    feat: заменил моковые заметки на firestore

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -1,15 +1,55 @@
-import { Outlet } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { useDisclosure } from '@mantine/hooks';
 import { AppShell, ScrollArea, Burger, Group, Skeleton, Card, Text } from '@mantine/core';
 import { Header } from '../Header';
 
-import data from '../../data.json';
+import { collection, getDocs } from "firebase/firestore"; 
+import { db } from '../../firebase';
+import { useAuth } from '../../context/AuthProvider';
 
+interface Note {
+  id: string;
+  data: {
+    header: string,
+    text: string,
+    changed: {
+      nanoseconds: number,
+      seconds: number
+    }
+  }
+}
 
 export function Shell() {
   const [opened, { toggle }] = useDisclosure();
+  const [notes, setNotes] = useState<Note[] | []>([]);
+  const navigate = useNavigate();
+  const auth = useAuth();
 
-  console.log(data)
+  const uid = auth.user.uid;
+
+  const fetchData = useCallback(async () => {
+    const querySnapshot = await getDocs(collection(db, "users", uid, "notes"));
+    const data: Note[] = [];
+
+    querySnapshot.forEach((doc) => {
+      const note = { id: doc.id, data: doc.data() } as Note;
+      data.push(note);
+    });
+
+    setNotes(data);
+  }, [uid])
+
+  useEffect(() => {
+    fetchData()
+      .catch(console.error);
+  }, [fetchData])
+  
+  const handleClick = (id: string) => () => {
+    navigate(`/notes/${id}`);
+  };
+
+  console.log(notes)
 
   return (
     <AppShell
@@ -30,23 +70,30 @@ export function Shell() {
           scrollbarSize={10}
           scrollHideDelay={500}
         >
-          {data.notes
-            .map((note) => (
-              // <Skeleton key={note.id} h={28} mt="sm" />
-              <Card
-                key={note.id}
-                withBorder
-                px={15}
-                py={10}
-                mb="xs"
-                shadow="sm"
-                component="a"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-              >
-                <Text>{note.name}</Text>
-                <Text c="dimmed" size="sm">{note.date}</Text>
-              </Card>
-          ))}
+          {
+            notes.length !== 0 ? (
+              notes.map((note) => (
+                <Card
+                  key={note.id}
+                  withBorder
+                  px={15}
+                  py={10}
+                  mb="xs"
+                  shadow="sm"
+                  component="a"
+                  onClick={handleClick(note.id)}
+                >
+                  <Text>{note.data.header}</Text>
+                  <Text c="dimmed" size="sm">{new Date(note.data.changed.nanoseconds).toLocaleString()}</Text>
+                </Card>
+              ))) : (
+                Array(15)
+                  .fill(0)
+                  .map((_, index) => (
+                    <Skeleton key={index} h={28} mt="sm" animate={false} />
+                  ))
+              )
+          }
         </ScrollArea>
       </AppShell.Navbar>
       <AppShell.Main>
