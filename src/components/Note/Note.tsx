a1cb886e60aa37761b5e7a41f1e3d37c0e15a891
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-11-30 16:37:28 +0300

    feat: перенес кнопку добавления новой заметки в Shell

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,36 +1,18 @@
+import { useParams } from 'react-router-dom';
+import { doc, deleteDoc, setDoc, Timestamp } from "firebase/firestore";
+import { db } from '../../firebase';
+import { useAuthStateUser } from '../../context/AuthProvider';
 import { Button, Group, Text, Paper } from '@mantine/core';
 import { IconPencil, IconTrash } from '@tabler/icons-react';
 
-import { addDoc, collection, doc, deleteDoc, setDoc, Timestamp } from "firebase/firestore"; 
-import { db } from '../../firebase';
-import { useAuth } from '../../context/AuthProvider';
-
-import { useParams } from 'react-router-dom';
-
-
 export function Note() {
-  const auth = useAuth();
+  const [user] = useAuthStateUser();
 
   const { id } = useParams();
   console.log(id)
 
-  const uid = auth.user.uid;
+  const uid = user!.uid;
 
-  const handleAdd = async () => {
-    const note = {
-      header: "5",
-      text: "The earth of every kind bearing fruit with the seed in it. And God saw that the tree of life. And let the dry land Earth, and the tree of life also in the earth and no herb of the sea and over the face of the deep, while a wind from God swept over the night, and to separate the day and over the cattle, and over the cattle, and over every creeping thing that moves upon the earth. Then the LORD God had taken from the presence of the LORD God said, Let us make humankind in our image, according to our likeness; and let them be lights in the garden at the time of the LORD God had taken from the tree, and I ate. And God said, Let the waters swarm, and every winged bird of the air, and brought her to the man. God made the two great lights - the greater light to rule the day that the man should be alone; I will greatly increase your pangs in childbearing; in pain you shall not eat, for in the day and over all the days of your life. So God created the heavens and the earth of every kind, and everything that creeps on the earth.",
-      changed: Timestamp.fromDate(new Date())
-    };
-
-    try {
-      const docRef = await addDoc(collection(db, 'users', uid, 'notes'), note);
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }
-  
   const handleChange = async () => {
     const note = {
       header: "five note",
@@ -65,23 +47,12 @@ export function Note() {
           Редактировать
         </Button>
 
-        <Button
-          leftSection={<IconPencil size={20} />}
-          radius="xl"
-          variant="gradient"
-          aria-label="Gradient action icon"
-          gradient={{ from: 'blue', to: 'cyan', deg: 45 }}
-          onClick={handleAdd}
-        >
-          Добавить
-        </Button>
-
         <Button 
           leftSection={<IconTrash size={20} />}
           radius="xl"
           variant="gradient"
           aria-label="Gradient action icon"
-          gradient={{ from: 'cyan', to: 'blue', deg: 45 }}
+          gradient={{ from: 'yellow', to: 'pink', deg: 45 }}
           onClick={handleDelete}
         >
           Удалить
--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -1,10 +1,10 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, getDocs, Timestamp } from "firebase/firestore";
 import { useAuthStateUser } from '../../context/AuthProvider';
 import { db } from '../../firebase';
 import { useDisclosure } from '@mantine/hooks';
-import { AppShell, ScrollArea, Burger, Group, Skeleton, Card, Text } from '@mantine/core';
+import { AppShell, ScrollArea, Burger, Group, Skeleton, Card, Text,Button } from '@mantine/core';
 import { Header } from '../Header';
 
 interface Note {
@@ -50,6 +50,21 @@ export function Shell() {
 
   console.log(notes)
 
+  const handleAddNewNote = async () => {
+    const note = {
+      header: "5",
+      text: "The earth of every kind bearing fruit with the seed in it. And God saw that the tree of life. And let the dry land Earth, and the tree of life also in the earth and no herb of the sea and over the face of the deep, while a wind from God swept over the night, and to separate the day and over the cattle, and over the cattle, and over every creeping thing that moves upon the earth. Then the LORD God had taken from the presence of the LORD God said, Let us make humankind in our image, according to our likeness; and let them be lights in the garden at the time of the LORD God had taken from the tree, and I ate. And God said, Let the waters swarm, and every winged bird of the air, and brought her to the man. God made the two great lights - the greater light to rule the day that the man should be alone; I will greatly increase your pangs in childbearing; in pain you shall not eat, for in the day and over all the days of your life. So God created the heavens and the earth of every kind, and everything that creeps on the earth.",
+      changed: Timestamp.fromDate(new Date())
+    };
+
+    try {
+      const docRef = await addDoc(collection(db, 'users', uid, 'notes'), note);
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
   return (
     <AppShell
       header={{ height: 60 }}
@@ -69,17 +84,17 @@ export function Shell() {
           scrollbarSize={10}
           scrollHideDelay={500}
         >
-          <Card
-            withBorder
-            px={15}
-            py={10}
-            mb="xs"
-            shadow="sm"
-            component="a"
-            // onClick={handleClick(note.id)}
+          <Button
+            fullWidth
+            size="lg"
+            radius="xl"
+            variant="gradient"
+            aria-label="Gradient action icon"
+            gradient={{ from: 'blue', to: 'cyan', deg: 45 }}
+            onClick={handleAddNewNote}
           >
-            <Text>Добавить новую заметку</Text>
-          </Card>
+            Добавить новую заметку
+          </Button>
           {
             notes.length !== 0 ? (
               notes.map((note) => (
@@ -88,7 +103,7 @@ export function Shell() {
                   withBorder
                   px={15}
                   py={10}
-                  mb="xs"
+                  mt="xs"
                   shadow="sm"
                   component="a"
                   onClick={handleClick(note.id)}
