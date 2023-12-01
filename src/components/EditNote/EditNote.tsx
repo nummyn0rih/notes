9c9c5a2ec6ce88a774227d63c0fa7e310c26265d
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-12-01 16:36:01 +0300

    feat: перенес изменение заметки в соответствующий компонент

--- src/components/EditNote/EditNote.tsx
+++ src/components/EditNote/EditNote.tsx
@@ -1,14 +1,75 @@
-import { Paper, Stack, Textarea } from '@mantine/core';
+import { useParams } from 'react-router-dom';
+import { doc, collection, setDoc, Timestamp } from "firebase/firestore";
+import { useDocument } from 'react-firebase-hooks/firestore';
+import { db } from '../../firebase';
+import { useAuthStateUser } from '../../context/AuthProvider';
+
+import { Center, Loader, Paper, Stack, Textarea } from '@mantine/core';
+import { useState } from 'react';
 
 export function EditNote() {
+  const { id } = useParams();
+  const [user] = useAuthStateUser();
+  const uid = user!.uid;
+  const [value, loading, error] = useDocument(doc(collection(db, 'users', uid, 'notes'), id));
 
+  const [noteHeader, setNoteHeader] = useState(JSON.stringify(value?.data()!.header));
+  const [noteText, setNoteText] = useState(JSON.stringify(value?.data()!.text) || '');
+  // console.log('id => ', id, 'uid => ', uid)
+
+
+  if (value) {
+    // console.log(JSON.stringify(value.data()))
+  }
+
+  // JSON.stringify(value.data()!.header)
+  // JSON.stringify(value.data()!.text)
+
+  async function update(note) {
+    try {
+      const docRef = doc(db, 'users', uid, 'notes', id as string);
+      await setDoc(docRef, note);
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
+    const id = e.target.id;
+    const note = {
+      header: id === 'noteHeader' ? e.target.value : noteHeader,
+      text: id === 'noteText' ? e.target.value : noteText,
+      changed: Timestamp.fromDate(new Date())
+    };
+
+    if (id === 'noteHeader') {
+      setNoteHeader(e.target.value);
+    }
+
+    if (id === 'noteText') {
+      setNoteText(e.target.value);
+    }
+
+    update(note);
+  }
 
   return (
     <Paper shadow="sm" p="xl">
-      <Stack>
-        <Textarea autosize maxRows={1} size="xl" />
-        <Textarea autosize minRows={30} />
-      </Stack>
+      {error && <strong>Error: {JSON.stringify(error)}</strong>}
+      {loading && (
+        <Center style={{height: "100%"}}>
+          <Loader color="blue" size="xl" type="dots"/>
+        </Center>
+      )}
+      {value && (
+        <Stack>
+          <Textarea autosize maxRows={1} onChange={handleChange} value={noteHeader} size="xl" id='noteHeader' />
+          <Textarea autosize minRows={30} onChange={handleChange} value={noteText} id='noteText' />
+        </Stack>
+      )
+      }
     </Paper>
   )
 }
--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,33 +1,19 @@
-import { useParams } from 'react-router-dom';
-import { doc, deleteDoc, setDoc, Timestamp } from "firebase/firestore";
+import { useParams, useNavigate } from 'react-router-dom';
+import { doc, deleteDoc } from "firebase/firestore";
 import { db } from '../../firebase';
 import { useAuthStateUser } from '../../context/AuthProvider';
 import { Button, Group, Text, Paper } from '@mantine/core';
 import { IconPencil, IconTrash } from '@tabler/icons-react';
 
 export function Note() {
-  const [user] = useAuthStateUser();
-
+  const navigate = useNavigate();
   const { id } = useParams();
-  console.log(id)
-
+  const [user] = useAuthStateUser();
   const uid = user!.uid;
 
-  const handleChange = async () => {
-    const note = {
-      header: "five note",
-      text: "The earth of every kind bearing fruit with the seed in it. And God saw that the tree of life. And let the dry land Earth, and the tree of life also in the earth and no herb of the sea and over the face of the deep, while a wind from God swept over the night, and to separate the day and over the cattle, and over the cattle, and over every creeping thing that moves upon the earth. Then the LORD God had taken from the presence of the LORD God said, Let us make humankind in our image, according to our likeness; and let them be lights in the garden at the time of the LORD God had taken from the tree, and I ate. And God said, Let the waters swarm, and every winged bird of the air, and brought her to the man. God made the two great lights - the greater light to rule the day that the man should be alone; I will greatly increase your pangs in childbearing; in pain you shall not eat, for in the day and over all the days of your life. So God created the heavens and the earth of every kind, and everything that creeps on the earth.",
-      changed: Timestamp.fromDate(new Date())
-    };
-
-    try {
-      const docRef = doc(db, 'users', uid, 'notes', id as string);
-      await setDoc(docRef, note);
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }
+  const handleChange = () => {
+    navigate(`/notes/${id}/edit`);
+  };
 
   const handleDelete = async () => {
     await deleteDoc(doc(db, 'users', uid, 'notes', id as string));
