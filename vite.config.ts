fbfcc05050c743bda7bf3ad3d0480e2393311370
Ivan Malshchukov <nummyn0rih@gmail.com>
2023-12-17 16:33:40 +0300

    feat: добавил sw.js и подключил плагин VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,85 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'inline',
+      strategies: 'injectManifest',
+      srcDir: '/',
+      filename: 'sw.js',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        "name": "Notes",
+        "short_name": "Notes",
+        "theme_color": "#4dabf7",
+        "background_color": "#ffffff",
+        "display": "standalone",
+        "orientation": "landscape-primary",
+        "scope": "/",
+        "start_url": "/",
+        "icons": [
+          {
+            "src": "assets/icons/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "assets/icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "assets/icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "assets/icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "assets/icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "assets/icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "assets/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "assets/icons/icon-384x384.png",
+            "sizes": "284x284",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "assets/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          }
+        ]
+      }
+    }),
+  ],
 })
